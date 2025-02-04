6f8e266e326654b8924a708ca68be8a40b0649b3
agyoungs <alexyoungs@hatchbed.com>
2025-02-04 16:08:28 -0600

    Fix formatting (#2)
    * Fix CRLF
    
    * Used standard formatting
    
    * Bumped version

--- examples/1-Calibration/SprayerTrailerServoCalibration/SprayerTrailerServoCalibration.ino
+++ examples/1-Calibration/SprayerTrailerServoCalibration/SprayerTrailerServoCalibration.ino
@@ -7,23 +7,22 @@
 / Questions? Concerns? Bugs? email code@mymindsi.com
 /
 / This example is to be used to set the open position
-/ of the MINDS-i Sprayer Trailer servos attached to 
+/ of the MINDS-i Sprayer Trailer servos attached to
 / pin 2 and pin 3
 /***************************************************/
 
 Servo leftBase, rightBase;
 
 void setup() {
-// Defines the pins the servos and ESC are plugged into
-  leftBase.attach(2);
-  rightBase.attach(3);
+    // Defines the pins the servos and ESC are plugged into
+    leftBase.attach(2);
+    rightBase.attach(3);
 
-// Sets the starting position for the servos and ESC
-  leftBase.write(135);  //135 closed -  45 open
-  rightBase.write(45);  // 45 closed - 135 open
+    // Sets the starting position for the servos and ESC
+    leftBase.write(135); // 135 closed -  45 open
+    rightBase.write(45); // 45 closed - 135 open
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    // put your main code here, to run repeatedly:
 }
